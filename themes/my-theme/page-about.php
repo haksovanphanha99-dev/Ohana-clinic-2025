b47db1517b3b6b909829cfeb63de43204ae43412
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2025-12-30 16:11:28 +0700

    done

---
+++ themes/my-theme/page-about.php
@@ -0,0 +1,51 @@
+<?php
+get_header();
+?>
+<div class="our-team">
+    <h1>Our Team</h1>
+</div>
+<div class="bout-doctor">
+    <div class="information">
+        <h1>Hak sovanPhanha</h1>
+        <h2>I have experience from Japan 5 year <br>
+        and form greaduated from Tokyo</h2>
+        <p>My name is Hak Sovanphanha, and I am a medical doctor with five years <br>
+        of professional experience in Tokyo, Japan.
+During my time in Tokyo, I gained valuable hands-on <br>
+experience working in a modern medical environment, <br>
+focusing on patient care, accuracy, and professionalism.
+
+<br>
+I am committed to providing high-quality medical services and building strong, <br>
+respectful relationships with my <br>
+patients. My goal is to offer clear communication, <br>
+careful diagnosis, and compassionate care to everyone I serve.
+
+<br>
+I look forward to helping patients improve their health and well-being with <br>
+dedication and trust.My name is Hak Sovanphanha, and I am a medical doctor with <br>
+five years of professional experience in Tokyo, Japan.
+
+    </div>
+    <div class="photo">
+        <img src="<?= get_template_directory_uri(); ?>/image/unnamed.jpg" alt="">
+    </div>
+</div>
+<br>
+<br>
+<br>
+<div class="container10">
+    <div class="picture10">
+        <img src="<?= get_template_directory_uri(); ?>/image/c.jpeg" alt="">
+    </div>
+    <div class="information10">
+        <h1>Our Clinic is welcome all of you need us to help </h1>
+        <p>I look forward to helping patients improve their health and well-being with <br>
+dedication and trust.My name is Hak Sovanphanha, and I am a medical doctor with <br>
+five years of professional experience in Tokyo, Japan.</p>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
